d0260bdb8fce90b5061d98fb2ac9bb02918de161
Dai Le <dai.n.le@sjsu.edu>
2020-03-01 22:58:48 -0800

    First part of Tweeter

---
+++ Twitter/TweetCelTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TweetCelTableViewCell.swift
+//  Twitter
+//
+//  Created by Dai Le on 3/1/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCelTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileimageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
